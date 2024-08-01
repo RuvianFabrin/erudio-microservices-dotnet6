f859e349d193e896388c1efe88baedac1b15a6d9
Ruvian Souza <ruvian.souza@basisti.com.br>
2024-07-31 22:49:51 -0300

    Calculadora completa e Criando Person

---
+++ RestWithASPNETUdemyPerson/RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        List<Person> FindAll();
+        void Delete(Person person);
+        Person FindByID(long id);
+    }
+}
